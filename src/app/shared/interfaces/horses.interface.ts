0d688078eb3d20ba0ed7a1556296a92d4a516f91
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-04-01 17:41:16 +0200

    created the project structure

---
+++ src/app/shared/interfaces/horses.interface.ts
@@ -0,0 +1,10 @@
+export interface IHorseItem {
+  id: number;
+  name: string;
+  age: number;
+  breed: string;
+  jockey:string;
+  sex?: string;
+  odds: number;
+  race: string;
+}
---
+++ src/app/shared/interfaces/races.interface.ts
@@ -0,0 +1,9 @@
+export interface IRaceLiveVideo {
+  id: string;
+  name: string;
+  country: string;
+  track: string;
+  start_time: string;
+  url: string;
+  thumbnail: string;
+}
