dbb105e430be378ea7ea6a781b3d59349a14a507
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-04-04 22:39:40 +0200

    added the main layout with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'auth'},
+  {path: 'auth', loadChildren: () => import('./modules/authorization/authorization.module').then(m => m.AuthorizationModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
