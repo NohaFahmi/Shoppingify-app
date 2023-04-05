d96523a9e16e095e278d964fde778ce5c7ef730b
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-04-05 17:14:39 +0200

    - added login btn to login page - edit in styles for login page

--- src/app/modules/authorization/authorization.module.ts
+++ src/app/modules/authorization/authorization.module.ts
@@ -5,6 +5,7 @@ import { AuthorizationRoutingModule } from './authorization-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import {ChipsModule} from "primeng/chips";
+import {ButtonModule} from "primeng/button";
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import {ChipsModule} from "primeng/chips";
   imports: [
     CommonModule,
     AuthorizationRoutingModule,
-    ChipsModule
+    ChipsModule,
+    ButtonModule
   ]
 })
 export class AuthorizationModule { }
--- src/app/modules/authorization/components/login/login.component.ts
+++ src/app/modules/authorization/components/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm?:FormGroup;
+  private emailPattern = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$';
+  constructor(private formBuilder:FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+    })
+  }
+
+  ngOnInit(): void {}
 
 }
