aac26e4f5deab565bcdce2356b2a7fbfc0e0e141
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-04-08 16:40:32 +0200

    - added signup page ui & logic

--- src/app/modules/authorization/components/signup/signup.component.ts
+++ src/app/modules/authorization/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  signupForm:FormGroup;
+  private emailPattern = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$';
+  private passwordPattern = '^(?=.*[A-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-!@#$%^&*_~+/.])\\S{8,20}$';
+  constructor(private formBuilder:FormBuilder) {
+    this.signupForm = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.passwordPattern)])),
+      passwordConfirm: new FormControl('', Validators.compose([Validators.required, Validators.pattern(this.passwordPattern)])),
+    })
+  }
 
+  getFormValidationErrors(fieldControl: AbstractControl | null): number {
+    if (fieldControl?.touched) {
+      if (fieldControl?.errors) {
+        if (fieldControl?.errors['required']) {
+          return 1;
+        }
+        if (fieldControl?.errors['pattern'].requiredPattern == this.emailPattern) {
+          return 2;
+        }
+        if (fieldControl?.errors['pattern'].requiredPattern == this.passwordPattern) {
+          return 3;
+        }
+      }
+    }
+    return 0;
+  }
+  onSignup() {
+    //TODO: implement signup logic
+  }
 }
