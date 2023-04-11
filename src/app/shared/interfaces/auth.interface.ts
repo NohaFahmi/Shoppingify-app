4a4cc52c86f1acafb787e5ace94b4bc728c986c4
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-04-11 21:29:04 +0200

    - added interface for auth

---
+++ src/app/shared/interfaces/auth.interface.ts
@@ -0,0 +1,13 @@
+export interface IAuthInfo {
+  email: string;
+  password: string;
+}
+
+export interface IUserInfo {
+  uid: string;
+  email: string;
+  displayName: string;
+  photoURL: string;
+  emailVerified: boolean;
+}
+
