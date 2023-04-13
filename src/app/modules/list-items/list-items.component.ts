fe653a13bcb29e1d09a3fc984506792db036e3fe
Noha M <33437197+NohaaAa@users.noreply.github.com>
2023-04-13 15:36:36 +0200

    - edit in ui styles for list items page - add logic to list items page

--- src/app/modules/list-items/list-items.component.ts
+++ src/app/modules/list-items/list-items.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-list-items',
@@ -6,6 +6,28 @@ import {Component, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./list-items.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class ListItemsComponent {
+export class ListItemsComponent implements OnInit {
+
+  listOfItems: {[key: string]: {itemId: number; itemName: string;}[]} = {}
+  constructor() {
+  }
+  ngOnInit() {
+    this.listOfItems = {
+      "Fruit and vegetables": [
+        {itemId: 1, itemName: 'Avocado'},
+        {itemId: 1, itemName: 'Potato'},
+        {itemId: 1, itemName: 'Banana'},
+        {itemId: 1, itemName: 'Chicken 1kg'},
+        {itemId: 1, itemName: 'Watermelon'},
+        {itemId: 1, itemName: 'Piele De Sapo Melon'},
+      ],
+      "Meat and Fish": [
+        {itemId: 1, itemName: 'Chicken 1kg'},
+        {itemId: 1, itemName: 'Pork fillets 450g'},
+        {itemId: 1, itemName: 'Chicken leg box'},
+        {itemId: 1, itemName: 'Salmon'}
+      ]
+    }
+  }
 
 }
